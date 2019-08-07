5ebb9a37612f527b43a028ba2d683426954beb6d
ljp <17602617792@163.com>
2019-08-07 16:49:14 +0800

    修改文件

---
+++ manage-system/src/components/page/Donate.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item><i class="el-icon-lx-redpacket_fill"></i> 支持作者</el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div class="container"> 
+            <div class="plugins-tips">
+                <p>图片展示区:</p>
+              <img src="../../assets/img/1.jpeg" alt="">
+              <img src="../../assets/img/2.jpg" alt="">
+              <img src="../../assets/img/3.jpeg" alt="">
+              <img src="../../assets/img/4.jpeg" alt="">
+              <img src="../../assets/img/timg.jpg" alt="">
+            </div>
+        </div>
+
+    </div>
+</template>
+<script>
+    export default {
+        
+    }
+</script>
+<style>
+.plugins-tips img{
+    width: 25%;
+}
+</style>
